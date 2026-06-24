fca9bd73d641ed6e4267be5c8e385973a96d80dc
sahilsharma91156 <sahilsharma91156@gmail.com>
2026-06-24 20:04:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
+@app.route("/health", methods=["GET"])
 def home():
-    return "Secureway Statement Analyzer Running"
+    return "Secureway Statement Analyzer Running", 200
+
+@app.errorhandler(404)
+def not_found(e):
+    return "Secureway Statement Analyzer Running", 200
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
